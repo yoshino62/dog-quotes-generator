17295d29fe138acdb2be565080c32b024c9fc0df
yoshino62 <150293831+yoshino62@users.noreply.github.com>
2024-01-06 18:35:03 -0500

    Added the basic handling form and Typewriter  plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: "Happiness is a warm puppy.",
+    autoStart: true,
+    cursor: " ",
+    delay: 20,
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
