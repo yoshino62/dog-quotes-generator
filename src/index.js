e52fe60ae4011271287f56b69877f1cd19f762dd
yoshino62 <150293831+yoshino62@users.noreply.github.com>
2024-01-06 20:35:51 -0500

    Added AI API Integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-function generateQuote(event) {
-  event.preventDefault();
+function displayQuote(response) {
+  console.log("quote generated");
 
   new Typewriter("#quote", {
-    strings: "Happiness is a warm puppy.",
+    strings: response.data.answer,
     autoStart: true,
     cursor: " ",
     delay: 20,
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+
+  let instuctionsInput = document.querySelector("#user-intructions");
+  let apiKey = "903bbfab25a139539824b07e1tfo211c";
+  let context =
+    "You are a dog lover and know a lot of the quotes for dog lovers. Your mission is to generate a short quote in basic HTML form and separate each lines with <br />. Make sure to follow the user instructions and provide one quote. Do not provide more than one quote. DO NOT include a title. Sign the quote with 'SheCodes AI' inside a <strong> element at the end of the quote.";
+  let prompt = `User instructions: Generate a quote for a dog lover based on thier mood follow ${instuctionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating quote");
+  console.log(`prompt, ${prompt}`);
+  console.log(`context, ${context}`);
+
+  axios.get(apiUrl).then(displayQuote);
 }
 
 let quoteFormElement = document.querySelector("#quote-generator-form");
